1d078d6dfce3319ef92060688248c10bbc12716c
develoHAM <kdw980722@gmail.com>
2024-09-06 00:41:22 +0900

    chore(database): add store_branch, company table relation

--- src/databases/sql/tables/store_branch.sql
+++ src/databases/sql/tables/store_branch.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS store_branch (
     id INT AUTO_INCREMENT PRIMARY KEY,
     location VARCHAR(255) NOT NULL UNIQUE,
     store_manager INT,
-    FOREIGN KEY (store_manager) REFERENCES store_manager(id) ON UPDATE CASCADE ON DELETE SET NULL 
+    company INT,
+    FOREIGN KEY (store_manager) REFERENCES store_manager(id) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (company) REFERENCES company(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
