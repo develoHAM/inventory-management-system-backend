59709d79a581c04d71cae108a51bc98a484af725
develoHAM <kdw980722@gmail.com>
2024-09-06 00:37:35 +0900

    chore(database): add store_manager, company table relation

--- src/databases/sql/tables/store_branch.sql
+++ src/databases/sql/tables/store_branch.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS store_branch (
     location VARCHAR(255) NOT NULL UNIQUE,
     store_manager INT,
     FOREIGN KEY (store_manager) REFERENCES store_manager(id) ON UPDATE CASCADE ON DELETE SET NULL 
-)
+);
