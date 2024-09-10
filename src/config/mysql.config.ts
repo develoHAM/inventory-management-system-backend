c9a2c6e2efda7847476196e1360e75e17cca0c4a
develoHAM <kdw980722@gmail.com>
2024-09-11 02:04:24 +0900

    refactor(config): separate connection options and table initialization order into 2 distinct objects

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,22 +1,26 @@
-export const mysqlConfig = {
-	tableInitializationOrder: [
-		'product_brand.sql',
-		'product_category.sql',
-		'company.sql',
-		'inventory.sql',
-		'store_manager.sql',
-		'warehouse.sql',
-		'product.sql',
-		'store_branch.sql',
-		'user.sql',
-		'product_inventory.sql',
-	],
+import { ConnectionOptions } from 'mysql2';
 
-	connectionOptions: {
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE,
-		port: Number(process.env.MYSQL_PORT),
-	},
+type TableOrderType = string[];
+
+export const mysqlConfig: ConnectionOptions = {
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	port: Number(process.env.MYSQL_PORT),
+	connectionLimit: 10,
+	waitForConnections: true,
 };
+
+export const tableInitializationOrder: TableOrderType = [
+	'product_brand.sql',
+	'product_category.sql',
+	'company.sql',
+	'inventory.sql',
+	'store_manager.sql',
+	'warehouse.sql',
+	'product.sql',
+	'store_branch.sql',
+	'user.sql',
+	'product_inventory.sql',
+];
