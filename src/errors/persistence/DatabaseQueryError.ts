c8cdeb3ab5cbf2fba8846b0ed12bbb7331c4a746
develoHAM <kdw980722@gmail.com>
2024-09-11 02:26:51 +0900

    chore(errors): rename errors directory from 'database/' to 'persistence/'

---
+++ src/errors/persistence/DatabaseQueryError.ts
@@ -0,0 +1,11 @@
+export class DatabaseQueryError extends Error {
+	constructor(sql: string, values?: any[]) {
+		let message = `Cannot query database\nSQL: ${sql}`;
+		if (values) {
+			message += `\nvalues: ${values}`;
+		}
+		super(message);
+		this.name = this.constructor.name;
+		Object.setPrototypeOf(this, DatabaseQueryError.prototype);
+	}
+}
