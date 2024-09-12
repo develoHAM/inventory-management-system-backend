512da1a236e5f4097ac8f612abc97114a35ca90d
develoHAM <kdw980722@gmail.com>
2024-09-12 16:31:46 +0900

    chore(types): create new types for updating and creating models

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,3 +1,7 @@
 export interface IModel {
 	id: number;
 }
+
+export type createModel<ModelType extends IModel> = Omit<ModelType, 'id'>;
+
+export type updateModel<ModelType extends IModel> = Partial<Omit<ModelType, 'id'>>;
