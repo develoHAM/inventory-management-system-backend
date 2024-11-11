bcff431c1e3ebff8a05a8ebabfc5a4d990d4c341
develoHAM <kdw980722@gmail.com>
2024-11-11 15:45:51 +0900

    chore(types): rename root directly name from interfaces to types for all class types

--- src/types/IDatabase.ts
+++ src/types/IDatabase.ts
@@ -1,16 +1,22 @@
-import mysql from 'mysql2/promise';
-
-export interface IDatabaseConnection extends mysql.PoolConnection {}
+export interface IDatabaseConnection {
+	beginTransaction(): Promise<void>;
+	commit(): Promise<void>;
+	rollback(): Promise<void>;
+	execute<ReturnType>(sql: string, values: any[]): Promise<[ReturnType, any]>;
+	query<ReturnType>(sql: string): Promise<[ReturnType, any]>;
+	release(): void;
+}
 
 export interface IDatabase<QueryResultType, RowType, ConnectionType> {
 	connect(): Promise<boolean>;
 	getConnection(): Promise<ConnectionType>;
 	releaseConnection(connection: ConnectionType): void;
-	initializeTables(): Promise<boolean>;
 	beginTransaction(connection: ConnectionType): Promise<void>;
 	commitTransaction(connection: ConnectionType): Promise<void>;
 	rollbackTransaction(connection: ConnectionType): Promise<void>;
-
+	lockForUpdate(sql: string): string;
+	lockForShare(sql: string): string;
+	initializeTables(): Promise<boolean>;
 	get queryRows(): (sql: string, values?: any) => Promise<[RowType[], any[]]>;
 	get queryRowsAsArray(): (sql: string, values?: any) => Promise<[RowType[][], any[]]>;
 	get queryResult(): (sql: string, values?: any) => Promise<[QueryResultType, any[]]>;
